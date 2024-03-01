52b84668b65a9f198996d3dadb5584483ecea9f2
abduazim10 <abduazimit@gmail.com>
2024-03-01 16:45:27 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,11 @@
+
+alert(` ${name} salomalekum`)
+
+let ism = prompt('String typeda malumot kiritimg')
+let number = prompt('Number typeda malumot kiritimg')
+let bool = prompt('Boolean typeda malumot kiritimg')
+
+alert(`Siz kiritgan ${ism} String typega tegishli 
+Siz kiritgan ${number} Number typega tegishli
+Siz kiritgan ${bool} Boolean typega tegishli`)
+
